6a17e15edd686e86aa89460311b24bac1c7d42db
Caleb Shaw <124619458+cshaw101@users.noreply.github.com>
2024-05-26 16:01:06 -0400

    added react-router-dom and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import Shop from './Components/Store/Shop';
+import Cart from './Components/Cart/Cart';
+import Header from './Components/Header';
 
-
-function App() {
-
+const App = () => {
   return (
-    <>
-      <h1>Main Page</h1>
-      
-    </>
-  )
-}
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
